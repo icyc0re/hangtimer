02fabfe41ca2acfb2e7f9dfa649d454ff23fd9a4
icyc0re <tiziano.signo@gmail.com>
2021-03-27 22:25:22 +0100

    add bootstrap and simple bootstrap header to the site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+const APP_TITLE = 'Hangtimer';
 
 function App() {
   return (
     <div className="App">
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div className="container-fluid px-3">
+          <a href="/" className="navbar-brand my-2 h1">{ APP_TITLE }</a>
+        </div>
+      </nav>
+
 
     </div>
   );
